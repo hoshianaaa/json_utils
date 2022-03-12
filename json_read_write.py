b1386d694000077a2326f009e5e2fe7558c29f70
hoshianaaa <hoshina54321@gmail.com>
2022-03-12 22:47:25 +0900

    ファイル存在判定追加

--- json_read_write.py
+++ json_read_write.py
@@ -1,9 +1,12 @@
 import json
+import os
 
 def read_json_file(file_name):
-  with open(file_name, 'r') as f:
-    json_load = json.load(f)
-    return json_load
+  if os.path.exists(file_name):
+    with open(file_name, 'r') as f:
+      json_load = json.load(f)
+      return json_load, True
+  return None, False
 
 def write_json_file(file_name, dict_data):
   with open(file_name, 'w') as f:
@@ -14,14 +17,21 @@ if __name__ == '__main__':
   print(json.__version__)
 
   f_name = "test.json"
-  data = read_json_file(f_name)
+  data, read_sucess = read_json_file(f_name)
 
-  new_data = {"a":"10"}
-  data.update(new_data)
+  if read_sucess == False:
+    print("cannot find file")
+    data = {'a': '10', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6'}
+    write_json_file(f_name, data)
 
-  new_data = {"f":"6"}
-  data.update(new_data)
+  else:
 
-  f_name = "test2.json"
+    new_data = {"a":"10"}
+    data.update(new_data)
 
-  write_json_file(f_name, data)
+    new_data = {"f":"6"}
+    data.update(new_data)
+
+    f_name = "test2.json"
+
+    write_json_file(f_name, data)
